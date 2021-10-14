19e0d551718f70e1232eb686b075b269726b2b20
Kurt C. M. See <kurtcms@kurt-macbook-air.local>
2021-10-14 20:38:48 +0800

    Refactor as Python app with Docker and Compose

--- README.md
+++ README.md
@@ -1,11 +1,176 @@
 # Web Monitoring: Monitor Web Page for Changes
 
-This Bash script is designed to be executed by a task scheduler such as [cron](https://crontab.guru/) and it does the following:
+This Python app is containerised with [Docker Compose](https://docs.docker.com/compose/) for rapid and modular deployment that fits in any microservice architecture.
 
-1. Execute [curl](https://curl.se/docs/manpage.html) to download a copy of the web page of interest.
-2. Use [sha256sum](https://linux.die.net/man/1/sha256sum) to compute a SHA 256-bit checksum with the downloaded web page.
-3. Compare the checksum with a previous build if one exists and on mismatch use [mail](https://linux.die.net/man/1/mail) to send a notification email to the predefined recipient. Otherwise if no previous build exists, save the checksum for future matching.
+It does the following:
+
+1. Use the Python library, [Requests](https://github.com/psf/requests), to download a copy of the web page of interest;
+2. Use the [hashlib](https://docs.python.org/3/library/hashlib.html) module to compute a SHA 256-bit checksum with the downloaded web page; and
+3. Compare the checksum with a previous build, if one exists, and on mismatch save the checksum and a copy of the downloaded web page, before using the [smtplib](https://docs.python.org/3/library/smtplib.html) module to send a notification email to the predefined recipient if it is required. Otherwise if no previous checksum exists, save the checksum the downloaded web page for future matching and references.
 
 A detailed walk-though is available [here](https://kurtcms.org/web-monitoring-monitor-web-page-for-changes/).
 
 <img src="https://kurtcms.org/git/web-monitor/web-monitor-screenshot.png" width="550">
+
+## Table of Content
+
+- [Getting Started](#getting-started)
+  - [Git Clone](#git-clone)
+  - [Environment Variable](#environment-variables)
+  - [Crontab](#crontab)
+  - [Docker Container](#docker-container)
+	  - [Docker Compose](#docker-compose)
+	  - [Build and Run](#build-and-run)
+  - [Standalone Python Script](#standalone-python-script)
+    - [Dependencies](#dependencies)
+    - [Usage and Option](#usage-and-option)
+    - [Cron](#cron)
+- [Checksum and the Downloaded Web Page](#checksum-and-the-downloaded-web-page)
+- [Reference](#reference)
+
+## Getting Started
+
+Get started in three simple steps:
+
+1. [Download](#git-clone) a copy of the app;
+2. Create the [environment variables](#environment-variables) for email notification if needed and modify the [crontab](#crontab);
+3. [Docker Compose](#docker-compose) or [build and run](#build-and-run) the image manually to start the app, or alternatively run the Python script as a standalone service.
+
+### Git Clone
+
+Download a copy of the script with `git clone`
+```shell
+$ git clone https://github.com/kurtcms/web-monitor /app/
+```
+
+### Environment Variables
+
+If email notification is required, the app expects the SMTPS port number, SMTP server address, the notification receiver email address, the notification sender email address and password as environment variables in a `.env` file in the same directory.
+
+Create the `.env` file.
+
+```shell
+$ nano /app/.env
+```
+
+And define the variables accordingly.
+
+```
+EMAIL_SSL_PORT = 465
+EMAIL_SMTP_SERVER = 'smtp.kurtcms.org'
+EMAIL_SENDER = 'alert@kurtcms.org'
+EMAIL_RECEIVER = 'kurtcms@gmail.com'
+EMAIL_SENDER_PASSWORD = '(redacted)'
+```
+
+### Crontab
+
+By default the app is scheduled with [cron](https://crontab.guru/) to pull a copy of the web page and check for changes every 15 minutes, with `stdout` and `stderr` redirected to the main process for `Docker logs`.  
+
+Modify the `crontab` to feed in the URL of interest, and signify to the Python script whether email notification is needed. Change to a different schedule if required.
+
+```shell
+$ nano /app/crontab
+```
+
+And define the variables accordingly.
+
+```
+*/15 * * * * /usr/bin/python3 /app/web-monitor.py -u https://lookingglass.pccwglobal.com/ > /proc/1/fd/1 2>/proc/1/fd/2
+#
+# Usage: web-monitor.py [-e] -u <url>
+#
+# Option:
+#   -h
+#     Display usage
+#   -e, --email
+#     Send email notification for changes
+#   -u, --url
+#     The URL of interest
+```
+
+### Docker Container
+
+#### Docker Compose
+
+With Docker Compose, the container may be provisioned with a single command. Be sure to have Docker Compose [installed](https://docs.docker.com/compose/install/).
+
+```shell
+$ docker-compose up
+```
+
+Stopping the container is as simple as a single command.
+
+```shell
+$ docker-compose down
+```
+
+#### Build and Run
+
+Otherwise the Docker image can also be built manually.
+
+```shell
+$ docker build -t web-monitor /app/
+```
+
+Run the image with Docker once it is ready.  
+
+```shell
+$ docker run -it --rm --name web-monitor web-monitor
+```
+
+### Standalone Python Script
+
+#### Dependencies
+
+Alternatively the `web-monitor.py` script may be deployed as a standalone service. In which case be sure to install the following required libraries:
+
+1. [Requests](https://github.com/psf/requests)
+2. [Python-dotenv](https://github.com/theskumar/python-dotenv)
+
+```shell
+$ pip3 install requests python-dotenv
+```
+
+#### Usage and Option
+
+The script reads the URL of interest from the `-u` argument and sends email notification on changes if the `-e` option is given.
+
+```
+Usage: web-monitor.py [-e] -u <url>
+
+Option:
+  -h
+    Display usage
+  -e, --email
+    Send email notification for changes
+  -u, --url
+    The URL of interest
+```
+
+#### Cron
+
+It may then be executed with a task scheduler such as [cron](https://crontab.guru/) that runs it once every 15 minutes for example.
+
+```shell
+$ (crontab -l; echo "*/15 * * * * /usr/bin/python3 /app/web-monitor.py -u https://lookingglass.pccwglobal.com/") | crontab -
+```
+
+## Checksum and the Downloaded Web Page
+
+The SHA 256-bit checksum and the downloaded web page will be saved as separate files on a `Docker volume` that is mounted in the same directory of the `docker-compose.yml` file on the Docker host. If the Python script is run as a standalone service, the files will be in the same directory of the script instead.
+
+In any case, the files are stored under a directory by the URL of the web page of interest, with the checksum stored in a file directly under it and the downloaded web page in a nested directory named by the full date and time of the download to ease access.
+
+```
+.
+└── url/
+    ├── url-sha256hash
+    └── YYYY-MM-DD-HH-MM-SS/
+        └── index.html
+```
+
+## Reference
+
+- [Requests Documentation
+](https://docs.python-requests.org/en/latest/)
---
+++ web-monitor.py
@@ -0,0 +1,195 @@
+import requests
+import smtplib, ssl
+from sys import path, argv
+from getopt import getopt, GetoptError
+from textwrap import dedent
+from hashlib import sha256
+from os import mkdir, environ
+from dotenv import load_dotenv, find_dotenv
+from datetime import datetime
+
+class monitor:
+    ERR_USAGE = '''\
+    Usage: web-monitor.py [-e] -u <url>
+
+    Option:
+      -h
+        Display usage
+      -e, --email
+        Send email notification for changes
+      -u, --url
+        The URL of interest
+    '''
+    ERR_USAGE = dedent(ERR_USAGE)
+    '''
+    Usage example of this script
+    '''
+
+    ERR_INVALID_ENV = 'Problem locating the .env file'
+    '''
+    Error message to display when python-dotenv fails to read
+    environment variables
+    '''
+
+    def __init__(self, argv):
+        '''
+        Read the argument(s) supplied and set variables depending
+        on their values
+        '''
+        try:
+            opts, args = getopt(argv, 'heu:',['email', 'url='])
+        except GetoptError:
+            '''
+            Raise a system exit with the script usage
+            on error reading the arguments
+            '''
+            raise SystemExit(self.ERR_USAGE)
+
+        self.email_noti = False
+        for opt, arg in opts:
+            if opt == '-h':
+                raise SystemExit(self.ERR_USAGE)
+            elif opt in ('-e', '--email'):
+                self.email_noti = True
+            elif opt in ('-u', '--url'):
+                self.url = arg
+
+        if not hasattr(self, 'url'):
+            '''
+            Raise a system exit with the script usage in the
+            absence of an URL supplied by the -u or --url argument
+            '''
+            raise SystemExit(self.ERR_USAGE)
+
+        if self.email_noti == True:
+            '''
+            Construct the email message and read environment variables
+            with python-dotenv if email notification is requested
+            '''
+            self.email_msg = f'''\
+                Subject: {self.url} has been updated\n
+                Updates are stored in separated files'''
+            self.email_msg = dedent(self.email_msg)
+
+            if load_dotenv(find_dotenv()) == False:
+                '''
+                Raise a system exit on error reading the
+                environment variables
+                '''
+                raise SystemExit(self.ERR_INVALID_ENV)
+
+            try:
+                '''
+                Read and set the environment variables needed for
+                email notification
+                '''
+                self.email_sslp = environ['EMAIL_SSL_PORT']
+                self.email_smtp = environ['EMAIL_SMTP_SERVER']
+                self.email_sender = environ['EMAIL_SENDER']
+                self.email_receiver = environ['EMAIL_RECEIVER']
+                self.email_sender_pw = environ['EMAIL_SENDER_PASSWORD']
+            except KeyError as e:
+                '''
+                Raise a system exit on error reading the environment variables
+                '''
+                raise SystemExit(e)
+
+        '''
+        Download a copy of the URL and raise a system exit
+        on connection error
+        '''
+        try:
+            page = requests.get(self.url)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+        self.page_content = page.content
+
+        '''
+        Generate a SHA 256-bit checksum of the downloaded contents
+        '''
+        self.page_content_hash = sha256(self.page_content).hexdigest()
+
+        '''
+        Set the file name for the SHA 256-bit checksum file
+        '''
+        self.dir_name_url_domain = self.url.split('//')[-1].split('/')[0]
+        self.file_name_page_content_hash = self.dir_name_url_domain \
+                                            + '-sha256hash'
+
+    def match(self):
+        '''
+        Call the __write method to write the SHA 256-bit checksum
+        and a copy of the downloaded content if a previous checksum
+        is not found. Otherwise read and match the previous checksum
+        against the current one and on mismatch call the __write method
+        to overwrite the checksum file and write a copy of the downloaded
+        content. Call the __email method to send an email notification
+        if it is requested
+        '''
+
+        try:
+            mkdir(path[0] + '/' + self.dir_name_url_domain)
+        except FileExistsError:
+            pass
+        finally:
+            self.working_dir = path[0] + '/' + self.dir_name_url_domain + '/'
+
+        try:
+            with open(self.working_dir \
+            + self.file_name_page_content_hash,'r') as f:
+                if f.read() != self.page_content_hash:
+                    self.__write()
+                    if self.email_noti == True:
+                        self.__email()
+        except FileNotFoundError:
+            self.__write()
+
+    def __write(self):
+        '''
+        Write the SHA 256-bit checksum in a directory named by the
+        sanitised URL and the downloaded content in a nested directory
+        named by the full date and time now to ease access
+        .
+        └── url/
+            ├── url-sha256hash
+            └── YYYY-MM-DD-HH-MM-SS/
+                └── index.html
+        '''
+        try:
+            with open(self.working_dir \
+            + self.file_name_page_content_hash, 'x') as f:
+                f.write(self.page_content_hash)
+        except FileExistsError:
+            with open(self.working_dir \
+            + self.file_name_page_content_hash, 'w') as f:
+                f.write(self.page_content_hash)
+
+        time_now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        try:
+            working_dir_time_now = self.working_dir + time_now + '/'
+            mkdir(working_dir_time_now)
+        except FileExistsError:
+            pass
+
+        with open(working_dir_time_now + 'index.html', 'x') as f:
+            f.write(self.page_content.decode())
+
+    def __email(self):
+        '''
+        Send an email notification
+        '''
+        ssl_context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.email_smtp, self.email_sslp,
+        context=ssl_context) as server:
+            server.login(self.email_sender, self.email_sender_pw)
+            server.sendmail(self.email_sender, self.email_receiver,
+                            self.email_msg)
+
+if __name__ == '__main__':
+    '''
+    Create the object and subsequently call its corresponding
+    method for matching
+    '''
+    web = monitor(argv[1:])
+    web.match()
